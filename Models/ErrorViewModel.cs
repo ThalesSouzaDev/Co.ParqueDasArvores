beefdc044e1cd189fde481bfd068ab79c54bfd6a
Thales Silva Souza <42788314+Thalesholigari@users.noreply.github.com>
2021-01-29 08:10:00 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Co.ParqueDasArvores.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
